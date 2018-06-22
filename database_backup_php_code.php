cf8c9776ff947c2abc3a22f65f6de94a2c0b53d1
adminecho <mehul@echoinnovateit.com>
2018-06-22 17:35:58 +0530

    First Commit

---
+++ database_backup_php_code.php
@@ -0,0 +1,54 @@
+<?php
+
+
+backupDatabaseTables('localhost','root','','stroke');
+
+function backupDatabaseTables($dbHost,$dbUsername,$dbPassword,$dbName,$tables = '*'){
+    //connect & select the database
+    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName); 
+
+    //get all of the tables
+    if($tables == '*'){
+        $tables = array();
+        $result = $db->query("SHOW TABLES");
+        while($row = $result->fetch_row()){
+            $tables[] = $row[0];
+        }
+    }else{
+        $tables = is_array($tables)?$tables:explode(',',$tables);
+    }
+
+	$return='';
+    //loop through the tables
+    foreach($tables as $table){
+        $result = $db->query("SELECT * FROM $table");
+        $numColumns = $result->field_count;
+		//$return .= "DROP TABLE $table;";
+
+        $result2 = $db->query("SHOW CREATE TABLE $table");
+        $row2 = $result2->fetch_row();
+
+        $return .= "\n\n".$row2[1].";\n\n";
+
+        for($i = 0; $i < $numColumns; $i++){
+            while($row = $result->fetch_row()){
+                $return .= "INSERT INTO $table VALUES(";
+                for($j=0; $j < $numColumns; $j++){
+                    $row[$j] = addslashes($row[$j]);
+                    //$row[$j] = preg_replace("\n","\\n",$row[$j]);
+                    if (isset($row[$j])) { $return .= '"'.$row[$j].'"' ; } else { $return .= '""'; }
+                    if ($j < ($numColumns-1)) { $return.= ','; }
+                }
+                $return .= ");\n";
+            }
+        }
+
+        $return .= "\n\n\n";
+    }
+
+    //save file
+    $handle = fopen('db-backup-'.time().'.sql','w+');
+    fwrite($handle,$return);
+    fclose($handle);
+}
+?>
